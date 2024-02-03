2b3eaab6c3b17eb27fda35f109b7cf2673e89e71
MuhammadIbraAlfathar <ibraalfathar31@gmail.com>
2024-02-03 11:55:34 +0700

    edit index html and add navbar

--- controller/hello_world.go
+++ controller/hello_world.go
@@ -5,13 +5,7 @@ import "net/http"
 func HelloWorldController() func(writer http.ResponseWriter, request *http.Request) {
 
 	return func(writer http.ResponseWriter, request *http.Request) {
-		writer.Write([]byte("Hello World"))
+		_, _ = writer.Write([]byte("Hello World"))
 	}
 
-}
-
-func TestController() func(writer http.ResponseWriter, request *http.Request) {
-	return func(writer http.ResponseWriter, request *http.Request) {
-		writer.Write([]byte("TESTTTT"))
-	}
 }
--- controller/index_employee.go
+++ controller/index_employee.go
@@ -12,14 +12,14 @@ func NewIndexController() func(writer http.ResponseWriter, request *http.Request
 		fp := filepath.Join("views", "index.html")
 		files, err := template.ParseFiles(fp)
 		if err != nil {
-			writer.Write([]byte(err.Error()))
+			_, _ = writer.Write([]byte(err.Error()))
 			writer.WriteHeader(http.StatusInternalServerError)
 			return
 		}
 
 		err = files.Execute(writer, nil)
 		if err != nil {
-			writer.Write([]byte(err.Error()))
+			_, _ = writer.Write([]byte(err.Error()))
 			writer.WriteHeader(http.StatusInternalServerError)
 			return
 		}
