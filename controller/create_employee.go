64a14582717aa6fdafcdb68deaf8d25f21e6e34f
MuhammadIbraAlfathar <ibraalfathar31@gmail.com>
2024-02-03 14:18:25 +0700

    create condition in create_employe controller

--- controller/create_employee.go
+++ controller/create_employee.go
@@ -9,20 +9,26 @@ import (
 func NewCreateEmployeeController() func(writer http.ResponseWriter, request *http.Request) {
 
 	return func(writer http.ResponseWriter, request *http.Request) {
-		fp := filepath.Join("views", "create.html")
-		files, err := template.ParseFiles(fp)
-		if err != nil {
-			_, _ = writer.Write([]byte(err.Error()))
-			writer.WriteHeader(http.StatusInternalServerError)
-			return
+
+		if request.Method == "POST" {
+			_, _ = writer.Write([]byte("Test"))
+		} else if request.Method == "GET" {
+			fp := filepath.Join("views", "create.html")
+			files, err := template.ParseFiles(fp)
+			if err != nil {
+				_, _ = writer.Write([]byte(err.Error()))
+				writer.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+
+			err = files.Execute(writer, nil)
+			if err != nil {
+				_, _ = writer.Write([]byte(err.Error()))
+				writer.WriteHeader(http.StatusInternalServerError)
+				return
+			}
 		}
 
-		err = files.Execute(writer, nil)
-		if err != nil {
-			_, _ = writer.Write([]byte(err.Error()))
-			writer.WriteHeader(http.StatusInternalServerError)
-			return
-		}
 	}
 
 }
